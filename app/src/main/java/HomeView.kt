ad23151889f196b09a12826862acac029a1b4010
Siddharth kote <siddharthkote128@gmail.com>
2024-12-22 19:54:51 +0530

    Fifith commit

--- app/src/main/java/HomeView.kt
+++ app/src/main/java/HomeView.kt
@@ -2,26 +2,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,7 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -46,8 +39,12 @@ fun HomeView() {
     var WeightDropdownMenuExpand by remember { mutableStateOf(false) }
 
     //Units
-    var HUnit by remember { mutableStateOf("Unit") }
+    var HUnit by remember {mutableStateOf("Unit")}
     var WUnit by remember {mutableStateOf("Unit")}
+
+    //Feet and Inches for height
+    var feet by remember {mutableStateOf("")}
+    var Inches by remember {mutableStateOf("")}
 
     Scaffold(
         topBar = {
@@ -73,6 +70,28 @@ fun HomeView() {
         ) {
             Row(
             ) {
+                if(HUnit=="feet"){
+                    OutlinedTextField(
+                        value = feet,
+                        onValueChange = {
+                            feet =it
+                        },
+                        label = {
+                            Text(text ="Enter the feet")
+                        },
+                        modifier=Modifier.weight(1f)
+                    )
+                    OutlinedTextField(
+                        value = Inches,
+                        onValueChange = {
+                            Inches = it
+                        },
+                        label = {
+                            Text(text = "Enter the Inches")
+                        },
+                        modifier=Modifier.weight(1f)
+                    )
+                }
                 OutlinedTextField(
                     value = heightInput,
                     onValueChange = {
@@ -91,7 +110,7 @@ fun HomeView() {
                             Icons.Default.ArrowDropDown,
                             contentDescription = "you are good enough to know that"
                         )
-                            //  Text(HUnit="Unit")
+                              Text(text=HUnit)
                     }
                     DropdownMenu(
                         expanded = HeightDropdownMenuExpand,
@@ -102,13 +121,14 @@ fun HomeView() {
                             text = { Text("feet") },
                             onClick = {
                                 HeightDropdownMenuExpand = false
+                                HUnit = "feet"
                             }
                         )
                         DropdownMenuItem(
                             text ={Text("metres")},
                             onClick = {
                                 HeightDropdownMenuExpand =false
-                                text = ""
+                                HUnit = "metres"
                             }
                         )
                     }
@@ -131,12 +151,12 @@ fun HomeView() {
                     TextButton(onClick = {
                         WeightDropdownMenuExpand = true
                     }) {
-                        Text(text = "Unit")
+                       // Text(text = "Unit")
                         Icon(
                             Icons.Default.ArrowDropDown,
                             contentDescription = "you are good enough to know that"
                         )
-                        Text(text = "Unit")
+                        Text(text = WUnit)
                     }
                     DropdownMenu(
                         expanded = WeightDropdownMenuExpand,
@@ -147,12 +167,14 @@ fun HomeView() {
                             text = { Text("kg") },
                             onClick = {
                                 WeightDropdownMenuExpand =false
+                                WUnit = "kg"
                             }
                         )
                         DropdownMenuItem(
                             text ={Text("lbs")},
                             onClick = {
                                 WeightDropdownMenuExpand =false
+                                WUnit = "lbs"
                             }
                         )
                     }
